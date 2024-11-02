e1ac814c6f6230c449ab4c18a6d7d1d84ce79304
Nikita Makarov <nom2188@nyu.edu>
2024-11-02 11:56:41 -0400

    Create method for reset in AverageMeter class

--- Final Project/utils.py
+++ Final Project/utils.py
@@ -4,3 +4,13 @@ import numpy as np
 
 def DepthNorm(depth:float, maxDepth:float=1000.0) -> float:
     return maxDepth / depth
+
+class AverageMeter(object):
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
