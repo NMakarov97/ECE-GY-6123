3c31a6732b8da6683d206b0da62eccefdd9db80b
Nikita Makarov <nom2188@nyu.edu>
2024-11-02 12:02:57 -0400

    Create method for update in AverageMeter class

--- Final Project/utils.py
+++ Final Project/utils.py
@@ -14,3 +14,9 @@ class AverageMeter(object):
         self.avg = 0
         self.sum = 0
         self.count = 0
+
+    def update(self, val, n=1) -> None:
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
