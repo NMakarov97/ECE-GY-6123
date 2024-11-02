8b75faa37271c2a22a7b9f065610a0e1e8bc46c1
Nikita Makarov <nom2188@nyu.edu>
2024-11-02 11:05:01 -0400

    Add section to download training data

--- Final Project/GetTrainingData.py
+++ Final Project/GetTrainingData.py
@@ -11,3 +11,12 @@ def download(link: str) -> str:
     Returns:
     path: final path of the downloaded file
     '''
+
+    path = "nyu_depth_v2.zip"
+    if os.path.isfile(path):
+        # Data zip file already exists
+        return path
+    else:
+        # Download the file
+        gdown.download(link, output=path)
+        return path
