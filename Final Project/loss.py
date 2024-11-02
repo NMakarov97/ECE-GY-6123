827ab922c99066a7489db1d583eb31835b78495c
Nikita Makarov <nom2188@nyu.edu>
2024-11-02 12:18:18 -0400

    Create function for create window

--- Final Project/loss.py
+++ Final Project/loss.py
@@ -10,3 +10,9 @@ def gaussian(window_size, sigma):
         ]
     )
     return gauss/gauss.sum()
+
+def create_window(window_size, channel=1):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
+    return window
