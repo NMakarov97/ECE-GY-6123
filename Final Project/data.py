287638cdf69ccf54c900727db873077a9b70dc48
Nikita Makarov <nom2188@nyu.edu>
2024-11-02 15:51:09 -0400

    Add function to check if image is numpy format

--- Final Project/data.py
+++ Final Project/data.py
@@ -2,6 +2,10 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, utils
 import torch
 from PIL import Image
+import numpy as np
 
-def _is_pil_image(img):
+def _is_pil_image(img) -> bool:
     return isinstance(img, Image.Image)
+
+def _is_numpy_image(img) -> bool:
+    return isinstance(img, np.ndarray)
