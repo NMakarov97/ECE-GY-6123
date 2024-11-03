e25dc50dfefc48bc1e5f054469a4c945cd44d842
Nikita Makarov <nom2188@nyu.edu>
2024-11-02 21:12:34 -0400

    Add typing to AverageMeter class methods

--- Final Project/utils.py
+++ Final Project/utils.py
@@ -18,7 +18,7 @@ class AverageMeter(object):
         self.sum = 0
         self.count = 0
 
-    def update(self, val, n=1) -> None:
+    def update(self, val:float, n:int=1) -> None:
         self.val = val
         self.sum += val * n
         self.count += n
