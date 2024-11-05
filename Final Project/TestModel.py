307d9d3cf4cbc33f68b73bb712bfef42bd4836f2
Nikita Makarov <nom2188@nyu.edu>
2024-11-05 16:13:17 -0500

    Create argument for checkpoint

--- Final Project/TestModel.py
+++ Final Project/TestModel.py
@@ -1,5 +1,14 @@
-def main() -> None:
+import argparse
 
+def main() -> None:
+    # Command line arguments
+    parser = argparse.ArgumentParser(description='Monocular Depth Estimation Testing')
+    parser.add_argument(
+        '--checkpoint',
+        '-c',
+        type=str,
+        help='path to checkpoint'
+    )
 
 if __name__ == '__main__':
     main()
